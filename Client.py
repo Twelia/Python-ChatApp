2813cb42ed100a45177f5f51d856e172bbe9172a
Bruno Farjallah <brunofarjallah@nixers.net>
2017-07-16 12:56:11 +0300

    Revert "Testing with OOP"
    This reverts commit 7444fb5a4d61475d4cfe118db9748848808527d9.

--- Client.py
+++ Client.py
@@ -1,58 +1,20 @@
-from socket import socket, AF_INET, SOCK_DGRAM, SHUT_RDWR
-from Crypto.Hash import SHA256
-from random import randint
-from sys import argv
-import json, sys, thread
-
-from AESCipher import *
-from ClientRegistry import *
-
-
 class Client:
-    shared_prime = 2**1536 - 2**1472 - 1 + 2**64 * ((2**1406) + 741804)
-    shared_base = 7
-
     def __init__(self, name):
         self.port = 12345
         self.size = 2**16
         self.IP = 'localhost'
         self.recipient = ''
         self.username = name
-        self.X = randint(30, 80)
-        self.Y = (shared_base ** X) % shared_prime
-        self.K = ''
-        self.socket = socket()
-        self.socket.connect((IP, port))
-        self.registry = ClientRegistry()
-
 
     # methods
 
-    def connect_server():
-        msg = json.dumps({'sharedkey': str(Y), 'sender': username})
-        self.socket.send(msg)
-        msg = json.loads(self.socket.recv(self.size))
-        Ys = long(msg['sharedkey'])
-        self.K = (Ys**X) % shared_prime
-        h = SHA256.new()
-        h.update(str(self.K))
-        self.K = h.hexdigest()
-
-    def send(msg):
-        msg = json.dumps(msg)
-        cipher = AESCipher(K)
-        msg = cipher.encrypt(msg)
-        s.send(msg)
+    def send():
+        return
 
     def receive_loop():
-        while 1:
-            cipher = AESCipher(self.K)
-            data = cipher.decrypt(self.socket.recv(self.size))
-            msg = json.loads(data)
-            print msg
-        # more stuff
-
+        # check client.py
+        return
 
     def input_loop():
-
+        # idem
         return
--- Server.py
+++ Server.py
@@ -1,14 +1,5 @@
-from Crypto import Random
-from Crypto.Hash import SHA256
-from random import randint
-from socket import socket, gethostbyname
-import json, sys, thread
-
 class Server:
-    shared_prime = 2**1536 - 2**1472 - 1 + 2**64 * ((2**1406) + 741804)
-    shared_base = 7
-
-    def __init__(self, maxconnections):
+    def __init__(self):
         self.port = 12345
         self.size = 2**16
         self.username_max_size = 50
@@ -21,20 +12,12 @@ class Server:
             '/send'     : send_to,
             '/exit'     : exit_client
         }
-        self.X = randint(30, 80)
-        self.Y = (shared_base ** X) % shared_prime
-        self.socket = socket()
-        self.registry = ServerRegistry()
-        self.conn = maxconnections # maximum client connections at a time
 
     # add methods accordingly to the commands above
     # check server.py for methods implementation
 
-    def send(msg):
-        key = connected_sockets[msg['receiver']]['private_key']
-        cipher = AESCipher(key)
-        msg = cipher.encrypt(json.dumps(msg))
-        self.socket.send(msg)
+    def send():
+        return
 
     def set_username():
         return
